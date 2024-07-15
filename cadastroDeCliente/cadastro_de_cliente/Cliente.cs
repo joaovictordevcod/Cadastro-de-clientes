71eb1dbb0d35b0ef7beefb1ac7937692245bf45c
João Victor Borges Tavares <110401685+joaovictordevcod@users.noreply.github.com>
2024-07-15 11:30:26 -0300

    Add files via upload
    #Código para cadastro de clientes em C#

---
+++ cadastroDeCliente/cadastro_de_cliente/Cliente.cs
@@ -0,0 +1,12 @@
+namespace Cadastro; 
+
+public class Cliente
+{
+      
+      public int Id{get;set;}
+      public String Nome{get;set;}
+      public DateOnly DataNascimento{get;set;}
+      public DateTime DataCadastro{get;set;}
+      public decimal Desconto{get;set;}
+
+}
